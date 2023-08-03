c7d497b0cbce4e33c41c7829dd9a7144981670eb
namodya <136572480+namodya@users.noreply.github.com>
2023-08-04 02:43:36 +0530

    Add files via upload

---
+++ Lab 04/Main 01/Q3/Q3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            // Ask for the kilometer input
+            Console.Write("Please enter the distance in kilometers: ");
+            string kmInput = Console.ReadLine();
+
+            // Parse the input to a double
+            if (double.TryParse(kmInput, out double kilometers))
+            {
+                // Create an object of the ConvertValues class
+                ConvertValues converter = new ConvertValues();
+
+                // Call the KilometerToMeter method with the kilometers as a parameter and store the result
+                double meters = converter.KilometerToMeter(kilometers);
+
+                // Display the result
+                Console.WriteLine($"{kilometers} kilometers is equal to {meters} meters.");
+            }
+            else
+            {
+                Console.WriteLine("Error: Invalid input. Please enter a valid number for kilometers.");
+            }
+
+            // Keep the console window open until a key is pressed
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
